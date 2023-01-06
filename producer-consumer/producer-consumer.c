9b444bf41cf491a25ecd4e9701d6935b53f62104
joaoafonso07 <joaoafonsomestre@gmail.com>
2023-01-06 23:41:28 +0000

    Update producer-consumer.c
    Started to implement pcq_create

--- producer-consumer/producer-consumer.c
+++ producer-consumer/producer-consumer.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
 #include"producer-consumer.h"
 #include<pthread.h>
+#include<stdlib.h>
 
+/*returns 0 if sucessful, -1 otherwise*/
 int pcq_create(pc_queue_t *queue, size_t capacity){
+    queue = (pc_queue_t*)realloc(queue, sizeof(void*)*capacity);
+    if(queue == NULL)
+        return -1;
+    return 0;
+}
+
+int pcq_destroy(pc_queue_t *queue){
     (void) queue;
-    (void) capacity;
     return 0;
 }
