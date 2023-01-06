b48b8394b626f872f43aa5936a90d33d2cf8cce3
joaoafonso07 <joaoafonsomestre@gmail.com>
2023-01-06 23:43:21 +0000

    Started pub.c
    Just opens the register_pipe yet

--- publisher/pub.c
+++ publisher/pub.c
@@ -1,9 +1,18 @@
 #include "logging.h"
+#include <errno.h>
+#include <fcntl.h>
 
 int main(int argc, char **argv) {
     (void)argc;
     (void)argv;
     fprintf(stderr, "usage: pub <register_pipe_name> <box_name>\n");
     WARN("unimplemented"); // TODO: implement
+
+    int tx = open(argv[1], O_WRONLY);
+    if(tx == -1){
+        WARN("failed to open named pipe: %s", strerror(errno));
+        return -1;
+    }
+
     return -1;
 }
