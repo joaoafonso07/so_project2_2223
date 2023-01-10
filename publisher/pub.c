4b8282c3fb3a8356b06fdc23638e2c08f02374fe
joaoafonso07 <joaoafonsomestre@gmail.com>
2023-01-10 09:33:26 +0000

    Publisher updated
    I think publisher is fully implemented and tested.

--- publisher/pub.c
+++ publisher/pub.c
@@ -11,6 +11,8 @@
 
 #define MAX_PIPE_PATH_LEN 256
 #define MAX_BOX_NAME_LEN 32
+#define MAX_MESSAGE_LEN 1024
+
 
 int main(int argc, char **argv) {
     /*
@@ -21,58 +23,93 @@ int main(int argc, char **argv) {
     */
     if(argc != 4){
         PANIC("invalid comand to launch a publisher");
-        return -1;
     }
 
-    uint8_t op_code = 1;
-    printf("%d\n", op_code);
+    uint8_t op_code1 = 1;
+    uint8_t op_code9 = 9;
+    //printf("%d\n", op_code1);//debug
+
+    int pid = getpid();
+
+    printf("pid: %d\n", pid);
+    printf("pid size: %ld\n", sizeof(pid)/sizeof(int));
 
     char *register_pipe_name = argv[1];
-    char *pipe_name = argv[2];
+    char *original_pipe_name = argv[2];
     char *box_name = argv[3];
 
-    printf("argv[1] = %s\n", register_pipe_name);
-    printf("argv[2] = %s\n", pipe_name);
-    printf("argv[3] = %s\n", box_name);
+    char *new_pipe_name = (char*)malloc(strlen(original_pipe_name) + 8); // 8 is the maximum length of a pid (e.g. "4294967295")
 
-    int tx = open(register_pipe_name, O_WRONLY);
-    if(tx == -1){
+
+    //printf("original_pipe_name size: %ld\n", strlen(original_pipe_name)); //debug
+    snprintf(new_pipe_name, strlen(original_pipe_name) + 8, "%s%d", original_pipe_name, pid);
+    //printf("new_pipe_name = %s\n", new_pipe_name); //debug
+    
+    //printf("argv[1] = %s\n", register_pipe_name); //debug
+    //printf("argv[2] = %s\n", original_pipe_name); //debug
+    //printf("argv[3] = %s\n", box_name); //debug
+
+    int register_fd = open(register_pipe_name, O_WRONLY);
+    if(register_fd == -1){
         PANIC("failed to open named pipe");
-        return -1;
     }
 
     /*buffer where we are going to put togheter a request to the mbroker*/
     char request[1 + MAX_PIPE_PATH_LEN + MAX_BOX_NAME_LEN];
 
-    printf("request 1: %s\n", request); //debug
+    //printf("request 1: %s\n", request); //debug
 
     memset(request, 0, 1 + MAX_PIPE_PATH_LEN + MAX_BOX_NAME_LEN); //puts all the buffer to '\0'
 
-    printf("request 2: %s\n", request); //debug
+    //printf("request 2: %s\n", request); //debug
 
-    memcpy(request, &op_code, sizeof(uint8_t)); //copy the code of the operation (1) to the start of the buffer
+    memcpy(request, &op_code1, sizeof(uint8_t)); //copy the code of the operation (1) to the start of the buffer
 
-    printf("request 3: %s\n", request); //debug
+    //printf("request 3: %s\n", request); //debug
 
-    strncpy(request + 1, pipe_name, MAX_PIPE_PATH_LEN - 1);
+    strncpy(request + 1, new_pipe_name, MAX_PIPE_PATH_LEN - 1);
 
-    printf("request 4: %s\n", request); //debug
+    //printf("request 4: %s\n", request); //debug
 
     strncpy(request + 1 + MAX_PIPE_PATH_LEN, box_name, MAX_BOX_NAME_LEN - 1);
 
-    printf("request 5: %s\n", request); //debug
+    //printf("request 5: %s\n", request); //debug
+    /*
+    if(write(1, request, 1 + MAX_PIPE_PATH_LEN + MAX_BOX_NAME_LEN) < 0)
+        PANIC("debug");
+    */
 
-    if(write(tx, request, 1 + MAX_PIPE_PATH_LEN + MAX_BOX_NAME_LEN) < 0){
+    if(write(register_fd, request, 1 + MAX_PIPE_PATH_LEN + MAX_BOX_NAME_LEN) < 0){
         PANIC("error writing request to register pipe")
         return -1;
     }
 
+    close(register_fd);
+    
+    unlink(new_pipe_name);
 
-    printf("yaaa\n");
+    if(mkfifo(new_pipe_name, 0660) == -1) {
+        PANIC("error creating pub_pipe");
+    }
+    
+    int pub_fd = open(new_pipe_name, O_WRONLY);
+    if(pub_fd == -1){
+        PANIC("failed to open named pipe");
+    }
+    
+    char message[1 + MAX_MESSAGE_LEN];
+    memcpy(message, &op_code9, sizeof(uint8_t));
+    
+    for (;;){
+        if(fgets(message + 1, MAX_MESSAGE_LEN, stdin) == NULL) //when receives EOF it returns NULL
+            break;
 
-    close(tx);
 
+        if(write(pub_fd, message, 1 + MAX_MESSAGE_LEN) < 0)
+            PANIC("error writing request to  pub_pipe")
 
+    }
 
+    close(pub_fd);
     return 0;
 }
